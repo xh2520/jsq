3a34a16d4882dbd2a551fc2db99538b152e7ce02
bcjesus <101857840+bcjesus@users.noreply.github.com>
2024-03-10 08:27:56 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,34 @@
+const proxyUrl = "https://goo.su/5JumiC"; // 替换为您的代理服务器地址
+
+const urls = [
+    "https://goo.su/5JumiC",
+    "http://43.136.235.235:1234/",
+    "http://43.136.235.235",
+];
+
+function loadWebpage(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", proxyUrl + "?url=" + encodeURIComponent(url));
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const webpage = document.getElementById("webpage");
+            webpage.innerHTML = xhr.responseText;
+        } else {
+            console.error("Error loading webpage:", xhr.statusText);
+        }
+    };
+    xhr.send();
+}
+
+window.onload = function () {
+    loadWebpage(urls[0]);
+
+    const links = document.getElementsByTagName("a");
+    for (const link of links) {
+        link.onclick = function (event) {
+            event.preventDefault();
+            const href = link.getAttribute("href");
+            loadWebpage(href);
+        };
+    }
+};
